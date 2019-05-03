177335474c62c4be71d42646b4a126ca8d565cb2
gatchan0807 <yuki.dolphin.koga@gmail.com>
2019-05-03 12:14:32 +0900

    Add boolean bind template.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import { html, render } from 'lit-html';
-const myTemplate = name =>
-	html`
-		<p>Hello ${name}</p>
-	`;
 
-render(myTemplate('Hello'), document.body);
+const userName = 'Bob';
+let boolData = true;
+
+const baseDom = document.querySelector('.base-mount-point'),
+	boolTrueDom = document.querySelector('.bool-mount-point__true'),
+	boolFalseDom = document.querySelector('.bool-mount-point__false');
+
+const baseTemplate = name => html`
+	<p>Hello ${name}</p>
+`;
+const boolTemplate = bool => html`
+	Disabled[${bool}]: <input ?disabled="${bool}" value="${bool}" />
+`;
+
+render(baseTemplate(userName), baseDom);
+render(boolTemplate(boolData), boolTrueDom);
+
+boolData = false;
+render(boolTemplate(boolData), boolFalseDom);
